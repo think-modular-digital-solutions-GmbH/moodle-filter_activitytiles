ba9e47bdc450562731c6eb86b2b72d74adff0c73
sweber <stefan.weber@think-modular.com>
2025-03-04 12:45:15 +0100

    #5: make whole card clickable

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'filter_activitytiles';
 $plugin->requires  = 2021040100;
 
-$plugin->version   = 202502180001;
-$plugin->release = '4.1 - 1.0.5';
+$plugin->version   = 202503040001;
+$plugin->release = '4.1 - 1.0.6';
 $plugin->maturity = MATURITY_STABLE;
